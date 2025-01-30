327da14c9a01a2dbb8f032f8184fd42d1c7a82b2
Priya Loran <ploran@usc.edu>
2025-01-30 05:15:26 +0000

    all done part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -5,9 +5,126 @@
 
 #include "ulliststr.h"
 
+using namespace std;
+
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+  /*
+
+  ULListStr testList;
+
+  // This is for push back
+  cout << "PB: Size before: " << testList.size() << endl;
+
+  testList.push_back("first back String");
+  testList.push_back("second back String");
+
+  cout << testList.back() << " " << testList.front() << endl;
+  cout << "PB: Size after: " << testList.size() << endl;
+
+  // This is for push front
+  cout << "PF: Size before: " << testList.size() << endl;
+
+  testList.push_front("first front String");
+  testList.push_front("second front String");
+
+  cout << testList.back() << " " << testList.front() << endl;
+  cout << "PF: Size after: " << testList.size() << endl;
+
+  // This is for pop back
+  cout << "POB: Size before: " << testList.size() << endl;
+  
+  testList.pop_back();
+
+  cout << testList.back() << endl;
+  cout << "POB: Size after: " << testList.size() << endl;
+
+  // This is for pop front
+  cout << "POF: Size before: " << testList.size() << endl;
+  
+  testList.pop_front();
+
+  cout << testList.front() << endl;
+  cout << "POF: Size after: " << testList.size() << endl;
+
+  //cout << testList.get(3);
+  // cout << testList.back();
+
+  ULListStr testList2;
+  testList2.push_back("1");
+  testList2.push_back("2");
+  testList2.push_back("3");
+  testList2.push_back("4");
+  testList2.push_back("5");
+  testList2.push_back("6");
+  testList2.push_back("7");
+  testList2.push_back("8");
+  testList2.push_back("9");
+  testList2.push_back("10");
+  testList2.push_back("11");
+  testList2.push_back("12");
+
+  testList2.pop_back(); 
+  testList2.pop_front(); 
+  testList2.pop_front();
+
+
+  cout << testList2.get(0) << endl;
+  cout << testList2.get(1) << endl;
+  cout << testList2.get(2) << endl;
+  //cout << testList2.get() << endl;
+
+  */
+
+  // TEST(ListRemove, SongOfBackAndFront)
+	ULListStr list;
+
+	list.push_back("oldfront");
+	list.push_back("oldback");
+
+	// EXPECT_TRUE(checkListContent(&list, {"oldfront", "oldback"}));
+  cout << list.get(0) << endl;
+  cout << list.get(1) << endl;
+
+	// this should create an empty slot at the front of the list, and the new element can be put there
+	list.pop_front();
+	list.push_front("newfront");
+
+	// EXPECT_TRUE(checkListContent(&list, {"newfront", "oldback"}));
+  cout << list.get(0) << endl;
+  cout << list.get(1) << endl;
+
+	// now, a new element struct should get created at the front
+	list.pop_back();
+	list.push_front("cloudfront");
+
+	// EXPECT_TRUE(checkListContent(&list, {"cloudfront", "newfront"}));
+  cout << list.get(9) << endl;
+  cout << list.get(10) << endl;
+  // Should be Bad Location
+  // cout << list.get(11) << endl;
+
+	// now, the original head should get deleted
+	list.pop_back();
+
+	// EXPECT_TRUE(checkListContent(&list, {"cloudfront"}));
+  // Should be a Bad Location
+  // cout << list.get(10) << endl;
+
+  // the whole list should be empty
+	list.pop_front();
+
+	// EXPECT_TRUE(checkListContent(&list, {}));
+  cout << list.empty() << endl;
+  // Should output 1 or True
+  // cout << list.get(9)
+
+  // a new and only element added
+	list.push_front("newback");
+
+	// EXPECT_TRUE(checkListContent(&list, {"newback"}));
+  cout << list.get(0) << endl;
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -25,6 +26,181 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+
+// 1. push back: Adds a new value to the back of the list O(1)
+void ULListStr::push_back(const std::string& val) {
+  // First check that the list is empty from the back
+  if (tail_ == nullptr) {
+    // If it is empty, we allocate another node item
+    // Size will be updated at the end
+    head_ = tail_ = new Item();
+  }
+  // Also check if the list is full from the back
+  // Since there are 10 items, last would point to index 10
+  if (tail_->last == 10) {
+    Item* add = new Item();
+    add->prev = tail_;
+    tail_->next = add;
+    tail_ = add;
+  }
+  // Update last to the next index of the array after we established
+  // of we were at the end or not
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+// 2. push front: Adds a new value to the front of the list O(1)
+void ULListStr::push_front(const std::string& val) {
+  // First check if the list is empty from the front
+  if (head_ == nullptr) {
+    // Did this a little different
+    // Add a new node item, manually assign first and last variables
+    // with values in this new node, and increase size because we have
+    // another value
+    head_ = tail_ = new Item();
+    head_->val[head_->first] = val;
+    head_->last++;
+    size_++;
+    return;
+  }
+  // Then check that the head item has no room before the first item
+  // Since first points to the first item, check if it is at index zero
+  // Again, manually change the new node item's first and last to the
+  // last/one past the last of the new front node
+  if (head_->first == 0) {
+    Item* add = new Item();
+    add->val[9] = val;
+    add->first = 9;
+    add->last = 10;
+    add->next = head_;
+    head_->prev = add;
+    head_ = add;
+    size_++;
+
+    /*
+    Debugging code:
+    std::cout << "changed1";
+    std::cout << head_->first;
+    */
+
+  }
+  // Update first to the first index that there is a value
+  else  {
+    head_->first--;
+    head_->val[head_->first] = val;
+    size_++;
+  }
+}
+
+// 3. pop front: Removes a value from the front of the list O(1)
+void ULListStr::pop_front() {
+  // Check that the list is not empty all together, use given function
+  // We just return the list
+  if (empty())  {
+    return;
+  }
+  // Then check if individual nodes/items have empty arrays
+  // If the first and last index point to the same location, it is empty
+  // Since last is one past the last value index, update first
+  head_->first++;
+  if (head_->first == head_->last) {
+    // Here we must deallocate this item
+      Item* temp = head_;
+      head_ = head_->next;
+      // This may have been the only item, so fix head and tail_
+      // to null because list is now empty
+      if (head_ == nullptr) {
+        tail_ = nullptr;
+      }
+      else  {
+        head_->prev = nullptr;
+      }
+      delete temp; 
+  }
+  size_--;
+}
+
+// 4. pop back: Removes a value from the back of the list O(1)
+void ULListStr::pop_back() {
+  // Check that the list is not empty all together
+  // We just return the list
+  if (empty())  {
+    return;
+  }
+  // Check that the array is not empty
+  // Since first is on the actually index value, update last
+  tail_->last--;
+  if (tail_->first == tail_->last) {
+    // Here we must deallocate this item
+      Item* temp = tail_;
+      tail_ = tail_->prev;
+      // If after deallocation, tail is null, the list must be empty
+      // so update head
+      if (tail_ == nullptr) {
+        head_ = nullptr;
+      }
+      else  {
+        tail_->next = nullptr;
+      }
+      delete temp; 
+  }
+  size_--;
+}
+
+// 5. back: Returns a const reference to the back element O(1)
+std::string const & ULListStr::back() const  {
+  // On the slides it says we can assume the list is not empty
+  std::string const& lastItem = tail_->val[tail_->last - 1];
+  return lastItem;
+}
+
+// 6. front: Returns a const reference to the front element O(1)
+std::string const & ULListStr::front() const  {
+  // On the slides it says we can assume the list is not empty
+  std::string const& firstItem = head_->val[head_->first];
+  return firstItem;
+}
+
+// 7. getValAtLoc: Returns a pointer to the item at index, loc,
+// if loc is valid and NULL otherwise O(n)
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  // We must check for cases that the location is above 9 since 
+  // the max size of an array in a node is 10
+
+  // First check if the list is empty
+  if (empty())  {
+    return NULL;
+  }
+
+  // The specific index of the given node of the location
+  int indexNum = 0;
+  // Number of elements traversed
+  int count = 0;
+
+  Item* temp = head_;
+
+  // Go through each node until the end is hit 
+  // in which case the location is not valid
+  while (temp != nullptr) {
+    // Tells us how many nodes there are to determine
+    // valid location because it is used in an expression to
+    // calculate is loc is in bounds
+    int nodeCount = temp->last - temp->first;
+
+    // This means the location is in this node and
+    // is valid
+    if (loc < count + nodeCount)  {
+      indexNum = temp->first + (loc - count);
+      return &(temp->val[indexNum]);
+    }
+    // Otherwise, the location may be in another node
+    // or may not be valid
+    count += nodeCount;
+    temp = temp->next;
+  }
+  return nullptr;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
